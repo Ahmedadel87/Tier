89a0cfeba1f1ed86a177185ad4ab7ce6743207b2
Omar <ahmedadel4355@gmail.com>
2026-04-21 20:50:53 +0300

    Removed some comments.
    Signed-off-by: Omar <ahmedadel4355@gmail.com>

--- src/Semantic Analysis.cpp
+++ src/Semantic Analysis.cpp
@@ -7,30 +7,7 @@
 static const std::unordered_map<std::string, symbol> symbol_table;
 
 Semantic_AST declaration(Declaration_Node& parser_AST, const int current_scope_id){
-    /*
-    struct Declaration_Node{
-        std::string identifier;
-        TokenType type; //* Only TokenTypes that are types will be assigned here, ex: i32
-        ExprNode expr;
-        bool const_;
-    };
-    struct symbol{
-        AST org_AST;
-        int scope_id;
-        std::vector<int> parents_id;
-    };
-    struct DECLARATION{ 
-        TokenType type; //* Only TokenTypes that are types will be assigned here, ex: i32
-        std::string ident;
-        ExprNode expr;
-        bool is_mutable;
-        bool is_iterable;
-    };
-    struct AST {
-        AST_type type;
-        Node node;
-    };
-    */
+    // TODO: Symbol Table
    return Semantic_AST{AST_type::DECLARATION, DECLARATION{parser_AST.type, parser_AST.identifier, std::move(parser_AST.expr), false, false}};
 }
 
